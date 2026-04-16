53b22dd66ecf0dc4ba110c788a22d82aec942833
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-04-16 11:10:26 +0530

    implemented mistral

--- learning-code/index.js
+++ learning-code/index.js
@@ -1,7 +1,22 @@
-import {data} from "./temp.js"
-const project="gen Ai node";
+
+const project = "gen Ai node";
+import { Mistral } from "@mistralai/mistralai";
+
+// console.log(project);
+
+const apiKey =
+  process.env.MISTRAL_API_KEY || "bBPVtggnVNzDcZuJGOki5ToFNdmNnmrm";
+const model = "mistral-medium-latest";
+
+const client = new Mistral({ apiKey: apiKey });
+
+const chatResponse = await client.chat.complete({
+  model: model,
+  messages: [{ role: "user", content: "What is capital of india?" }],
+});
 
 
-console.log("this is data", data);
 
-console.log(project);
+console.log("this is model", chatResponse.choices[0]?.message);
+
+
