db605cae087a5d40959227235d7a9500b17844b4
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-14 15:20:38 +0530

    string done

---
+++ python-learning/a_basic_python/string.py
@@ -0,0 +1,24 @@
+name='Tausif'
+print(name)
+
+
+print("Tausif and Rizvi's family")
+
+print('This is rizvi\'s "Tausif"')
+
+print('Tausif ' * 10)
+
+print('c\\system\\tausif')
+
+print('Tausif' 'Hassan')
+
+print(name + "Hassan")
+
+print(name[0])
+
+# get last character 
+print(name[-1])
+
+# get chracter in range 
+
+print(name[0:3])
