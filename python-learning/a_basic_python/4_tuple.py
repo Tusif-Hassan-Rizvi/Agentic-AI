e57e728dc264622eb73fa87daa9a0d9b52d0fd82
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-14 16:53:25 +0530

    tuple done

---
+++ python-learning/a_basic_python/4_tuple.py
@@ -0,0 +1,29 @@
+# tupple are immutable 
+
+tup=[233, 33,63,632]
+
+type(tup)
+
+print(type(tup))
+
+# way of creating tuple 
+
+tup=66,222, 2
+
+print(type(tup))
+
+tup=(22,623,987)
+
+print(type(tup))
+
+
+tup=(22, "Tausif", 25.5)
+
+num, name, age =tup
+
+print(num, name, age)
+
+# check in tuple 
+
+print("Tausif" in tup)
+
