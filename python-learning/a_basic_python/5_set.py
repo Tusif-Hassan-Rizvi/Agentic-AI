cb2cbee0aaf5cd0e862a0db1cdd8e20c28afe8f1
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-14 17:08:38 +0530

    set done

---
+++ python-learning/a_basic_python/5_set.py
@@ -0,0 +1,32 @@
+# A set is a collection of unordered, unique elements.
+
+
+
+
+set1={23, 56, 78, 21, 56}
+
+
+print(set1)
+
+print(len(set1))
+print(type(set1))
+
+set2={}
+
+print(type(set2))
+
+
+# set function 
+
+set3=set("abcdmnop")
+set4=set("aeiou")
+
+print(set3)
+print(set4)
+
+
+print(set3-set4)
+
+a = set( "tausif" )
+b = set("hassan")
+print(a & b)
