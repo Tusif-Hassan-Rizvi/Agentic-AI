62a1e773c5447804122802a8dcf661fe5ad3f490
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-15 19:04:28 +0530

    updated change

---
+++ python-learning/a_basic_python/8_dataTypes.py
@@ -0,0 +1,24 @@
+x=10
+print(type(x))
+
+py=3.14
+
+print(type(py))
+
+name="Tusif"
+
+print(type(name))
+
+is_True=True
+
+print(type(is_True))
+
+result =None 
+# None is same as null in js 
+
+print(type(result))
+
+rangeType=range(10)
+print(rangeType)
+
+print(list(rangeType))
