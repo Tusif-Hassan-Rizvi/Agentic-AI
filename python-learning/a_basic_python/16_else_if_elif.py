bdcf79e4d4aace1fbae5449a249d1a70391586cd
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-16 16:51:45 +0530

    updated elif

---
+++ python-learning/a_basic_python/16_else_if_elif.py
@@ -0,0 +1,13 @@
+num=input("enter the number: ")
+
+num=int(num)
+
+if num==1:
+    print("number one")
+elif num==2:
+    print("number is two")
+elif num==3:
+    print("number is three")    
+else:
+    print("nothing is match")    
+
