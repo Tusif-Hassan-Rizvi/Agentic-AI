6b6a4fae1c9a6852ada1ac6c575891c7b713d5f8
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-16 15:35:03 +0530

    swap variable done

---
+++ python-learning/a_basic_python/12_swap-variable.py
@@ -0,0 +1,24 @@
+a=5
+b=6
+
+# swap using a extra variable 
+# c=a
+# a=b
+# b=c
+
+
+print(a, b)
+
+
+# swap using math formula 
+
+# a=a+b #11
+# b=a-b #5
+# a=a-b #6
+
+print(a, b)
+
+# simple python way 
+a,b=b,a
+
+print(a,b)
