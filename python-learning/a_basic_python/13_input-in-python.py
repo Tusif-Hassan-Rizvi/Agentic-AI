eb884861d4fbd4e8c5a323a19121150760c3f213
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-16 16:32:23 +0530

    if and else done

---
+++ python-learning/a_basic_python/13_input-in-python.py
@@ -0,0 +1,18 @@
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+
+# by default input gives and string 
+# if want to do number operation convert into the integar 
+
+a=int(a)
+b=int(b)
+
+
+c=a+b
+
+
+print("Your given value is: ", a, b, "and the sum is: ", c)
+
+name=input("Enter your name: ")[0]
+
+print("your name first corrector is: ", name)
---
+++ python-learning/a_basic_python/14_if_else_condition.py
@@ -0,0 +1,8 @@
+num=input("Enter the number: ")
+
+num=int(num)
+
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
