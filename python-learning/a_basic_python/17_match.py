eb02f99a83c4911baaec76b38510d71a615e60e8
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-17 12:01:25 +0530

    while loop done

---
+++ python-learning/a_basic_python/17_match.py
@@ -0,0 +1,15 @@
+num=input("enter the number: ")
+
+num=int(num)
+
+match num:
+  case 1:
+    print("One")
+  case 2:
+    print("two")
+  case 3:
+    print("Three") 
+  case _:
+    print("Nothing match")  
+   
+
---
+++ python-learning/a_basic_python/18_while-loop.py
@@ -0,0 +1,14 @@
+i=1
+
+
+while(i<=5):
+    j=1
+    print("You are right ", end="")
+    while(j<=5):
+        print("another one", end="")
+        j+=1
+    i+=1
+
+    print()
+
+
