3d9ff63eb5d59c3c892b150440a70f5bb9bbfd65
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-17 12:19:05 +0530

    break and continue done for loop done

---
+++ python-learning/a_basic_python/19_foor-loop.py
@@ -0,0 +1,22 @@
+data=['Tausif', 26, 56000, 'Delhi']
+
+
+counter=0
+while counter<len(data):
+    print(data[counter])
+    counter+=1
+
+
+
+
+for value in data:
+    print(value)
+
+
+
+for count in range(10):
+    if(count%3==0):
+        continue
+    if(count==8):
+        break
+    print(count)    
