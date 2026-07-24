f4f47c5a4418cce7902a86971d9cca4177e96eb0
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-07-24 12:29:42 +0530

    oops updated

---
+++ python-learning/a_basic_python/35_oops.py
@@ -0,0 +1,15 @@
+# Object-oriented programming uses classes, and these classes contain methods.
+
+class comptuer:
+    def config(self):
+        print("System bahut tagda hai bro...trust me.", self)
+
+
+comp1=comptuer()
+comp2=comptuer()
+
+# comptuer.config(comp1)
+# comptuer.config(comp2)
+
+comp1.config()
+comp2.config()
