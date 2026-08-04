0fbbf55d5709a12131e5a86507361cd33168f436
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-08-04 16:01:27 +0530

    created update and delete api

--- python-learning/fast_api/main.py
+++ python-learning/fast_api/main.py
@@ -19,7 +19,7 @@ products = [
 
 
 
-
+# get api 
 @app.get("/products")
 def get_all_product():     
     return products
@@ -33,9 +33,32 @@ def get_product_by_id(id:int):
     return "Product not found"
 
 
-
+# post api 
 @app.post("/product")
 def add_product(product:Product):
     products.append(product)
     return product
     
+    
+    
+# update api    
+@app.put("/product") 
+def update_product(id:int, product:Product):
+    for i in range(len(products)):
+        if products[i].id==id:
+            products[i]==product
+            return {"message": "Product updated successfully", "data": products[i]}
+          
+    return "No product found"        
+
+
+
+# delete api 
+@app.delete("/product")
+def delete_product(id: int):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i] 
+            return {"message": "Product deleted successfully", "data": products}
+            
+    return {"message": "No product found"}    
