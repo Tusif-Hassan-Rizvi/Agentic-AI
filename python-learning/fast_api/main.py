551dba0957a8379ebccc26b6554d3e934ed1c84e
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2026-08-04 12:40:59 +0530

    done fast api get and post and get by id

--- python-learning/fast_api/main.py
+++ python-learning/fast_api/main.py
@@ -23,3 +23,19 @@ products = [
 @app.get("/products")
 def get_all_product():     
     return products
+
+
+@app.get("/product/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id==id:
+            return product
+    return "Product not found"
+
+
+
+@app.post("/product")
+def add_product(product:Product):
+    products.append(product)
+    return product
+    
